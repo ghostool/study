c72b25737ff8a271994d2be1ce65ef0bf575b66b
chainstart <46473351+chainstart@users.noreply.github.com>
2019-01-25 21:14:02 +0800

    Update EOSBetDice.cpp

--- EOSBetDice/EOSBetDice.cpp
+++ EOSBetDice/EOSBetDice.cpp
@@ -631,9 +631,6 @@ extern "C" { \
    void apply( uint64_t receiver, uint64_t code, uint64_t action ) { \
       auto self = receiver; \
       if( code == self || code == N(eosio.token)) { \
-      	 /*if( action == N(transfer)){ \
-      	 	eosio_assert( code == N(eosio.token), "Must transfer EOS"); \
-      	 } \*/
          TYPE thiscontract( self ); \
          switch( action ) { \
             EOSIO_API( TYPE, MEMBERS ) \
@@ -653,3 +650,8 @@ EOSIO_ABI_EX( EOSBetDice,
 	(suspendbet)
 	(calcbetid)
 )
+	
+	
+	/*if( action == N(transfer)){ \
+      	 	eosio_assert( code == N(eosio.token), "Must transfer EOS"); \
+      	 } \*/
